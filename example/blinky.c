9daccf8e9fcf3052746a0bfbf78d00cb4d670834
Kamil Wcislo <kamil.wcislo@3mdeb.com>
2017-03-29 19:30:37 +0200

    example: add functioning blinky example
    Signed-off-by: Kamil Wcislo <kamil.wcislo@3mdeb.com>

---
+++ example/blinky.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <apu2_gpio.h>
+
+int main(void) {
+    int status, i;
+    unsigned val = 1;
+
+    status = apu_gpio_init();
+    if (status < 0) {
+        printf("GPIO library init failed: %d\n", status);
+        return -1;
+    }
+
+    //
+    // set led gpios direction -> out
+    //
+    for (i = 0; i < 3; i++) {
+        status = apu_gpio_set_dir(APU_LED1 + i, APU_DIR_OUT);
+        if (status < 0) {
+            printf("LED%d direction set failed: %d\n", i, status);
+        }
+    }
+
+    while (1) {
+        apu_gpio_set_val(APU_LED2, val);
+        val ^= 1;
+        apu_gpio_set_val(APU_LED3, val);
+        sleep(1);
+    }
+
+    return 0;
+}
