914d14920757dadf03bf1d39a43d08b83270626c
Kamil Wcislo <kamil.wcislo@3mdeb.com>
2017-03-29 19:30:11 +0200

    src: add all necessary gpio functions
    Signed-off-by: Kamil Wcislo <kamil.wcislo@3mdeb.com>

--- apu2_gpio.h
+++ apu2_gpio.h
@@ -5,10 +5,38 @@
 #ifndef APU2_GPIO_H
 #define APU2_GPIO_H
 
-#define APU_SUCCESS      0
-#define APU_IO_ERROR    -1
-#define APU_MAP_FAILED  -2
+//
+// Error codes returned by library
+//
+#define APU_SUCCESS      0  // success
+#define APU_IO_ERROR    -1  // i/o error opening mem file
+#define APU_MAP_FAILED  -2  // mem mapping failed
+#define APU_INV_PARAM   -3  // invalid parameter
+
+//
+// GPIO offsets definitions
+//
+#define APU_GPIO_57     0x44
+#define APU_GPIO_58     0x45
+#define APU_GPIO_59     0x46
+#define APU_GPIO_32     0x59
+#define APU_GPIO_33     0x5A
+#define APU_LED1        APU_GPIO_57
+#define APU_LED2        APU_GPIO_58
+#define APU_LED3        APU_GPIO_59
+#define APU_MODESW      APU_GPIO_32
+#define APU_SIMSWAP     APU_GPIO_33
+
+//
+// GPIO states
+//
+#define APU_DIR_IN      0x0
+#define APU_DIR_OUT     0x1
 
 int apu_gpio_init(void);
+int apu_gpio_get_dir(unsigned offset);
+int apu_gpio_set_dir(unsigned offset, unsigned direction);
+int apu_gpio_get_val(unsigned offset);
+int apu_gpio_set_val(unsigned offset, unsigned value);
 
 #endif
--- src/apu2_gpio.c
+++ src/apu2_gpio.c
@@ -2,6 +2,8 @@
  *
  */
 
+#include <stdint.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <unistd.h>
@@ -9,10 +11,14 @@
 #include <apu2_gpio.h>
 
 #define FCH_ACPI_MMIO_BASE      0xFED80000
-#define FCH_GPIO_BASE           (FCH_ACPI_MMIO_BASE + 0x1500)
+#define FCH_GPIO_OFFSET         0x1500
 #define FCH_GPIO_SIZE           0x300
 
-volatile unsigned *gpio;
+#define GPIO_BIT_DIR            23
+#define GPIO_BIT_WRITE          22
+#define GPIO_BIT_READ           16
+
+static volatile unsigned *gpio;
 
 int apu_gpio_init(void) {
 
@@ -27,12 +33,12 @@ int apu_gpio_init(void) {
     // mmap the GPIO space
     //
     gpio_map = mmap(
-        NULL,                   // Any adddress in our space will do
-        FCH_GPIO_SIZE,          // Map length
-        PROT_READ|PROT_WRITE,   // Enable reading & writting to mapped memory
-        MAP_SHARED,             // Shared with other processes
-        mem_fd,                 // File to map
-        FCH_GPIO_BASE           // Offset to GPIO peripheral
+        NULL,                               // Any adddress in our space will do
+        FCH_GPIO_OFFSET + FCH_GPIO_SIZE,    // Map length
+        PROT_READ|PROT_WRITE,               // Enable reading & writting to mapped memory
+        MAP_SHARED,                         // Shared with other processes
+        mem_fd,                             // File to map
+        FCH_ACPI_MMIO_BASE                  // Offset of the MMIO BASE
    );
 
    close(mem_fd); // No need to keep mem_fd open after mmap
@@ -41,6 +47,69 @@ int apu_gpio_init(void) {
       return APU_MAP_FAILED;
    }
 
-   // Always use volatile pointer!
-   gpio = (volatile unsigned *)gpio_map;
+   gpio = (volatile unsigned *)(gpio_map + FCH_GPIO_OFFSET);
+
+   return APU_SUCCESS;
+}
+
+static unsigned is_offset_valid(unsigned offset) {
+    if (offset > FCH_GPIO_SIZE) {
+        return 0;
+    }
+    return 1;
+}
+
+int apu_gpio_get_dir(unsigned offset)
+{
+    uint32_t val;
+
+    if (!is_offset_valid(offset))
+        return APU_INV_PARAM;
+
+    val = *(gpio + offset);
+    return (val >> GPIO_BIT_DIR) & 1;
+}
+
+int apu_gpio_set_dir(unsigned offset, unsigned direction)
+{
+    volatile uint32_t *val;
+
+    if (!is_offset_valid(offset))
+        return APU_INV_PARAM;
+
+    val = gpio + offset;
+    if (direction == APU_DIR_IN)
+        *val &= ~(1 << GPIO_BIT_DIR);
+    else
+        *val |= (1 << GPIO_BIT_DIR);
+
+    return APU_SUCCESS;
+}
+
+int apu_gpio_get_val(unsigned offset)
+{
+    uint32_t val;
+
+    if (!is_offset_valid(offset))
+        return APU_INV_PARAM;
+
+    val = *(gpio + offset);
+    return (val >> GPIO_BIT_READ) & 1;
+}
+
+int apu_gpio_set_val(unsigned offset, unsigned value)
+{
+    volatile uint32_t *val;
+
+    if (!is_offset_valid(offset))
+        return APU_INV_PARAM;
+
+    val = gpio + offset;
+
+    if (value)
+        *val |= (1 << GPIO_BIT_WRITE);
+    else
+        *val &= ~(1 << GPIO_BIT_WRITE);
+
+    return APU_SUCCESS;
 }
